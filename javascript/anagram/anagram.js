dfb1fc5e0adfa524a452065c5434b9c743abf26c
Drew Wyatt <Drew.Wyatt@activenetwork.com>
2015-08-08 12:05:30 -0500

    add some stuff to javascript anagram problem.

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -4,12 +4,32 @@
 	}
 	
 	var Anagram = function(word) {
-		this.word = word;
+		this._word = word.split('');
 	}
 	
 	Anagram.prototype.matches = function(words) {
+		var matches = [];
+		var self = this;
+		words.forEach(function(word) {
+			if(self._matchesThisWord(word)) {
+				matches.push(word);
+			}
+		});	
 		
+		return matches;
 	} 
+	
+	Anagram.prototype._matchesThisWord = function(word) {
+		var isMatch = false;
+		if(word.length === this._word.length) {
+			var wordToMatch = word.split('');
+			this._word.forEach(function(letter) {
+				console.log(wordToMatch.indexOf(letter));
+			});
+		}
+		
+		return isMatch;
+	}
 	
 	module.exports = Anagrammer;
 })();
