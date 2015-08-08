dfe1bcc8dc4da5f6ee653f0da40c2fe5f36af7f0
Drew Wyatt <Drew.Wyatt@activenetwork.com>
2015-08-08 18:01:12 -0500

    stubbed out BeerSong

---
+++ javascript/beer-song/beer-song.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  var BeerSong = function() {};
+
+  BeerSong.verse = function() {
+    var lyrics = '';
+    return lyrics;
+  };
+
+  BeerSong.sing = function() {
+    var lyrics = '';
+    return lyrics;
+  };
+
+  module.exports = BeerSong;
+})();
