0149c2bdbfc8064894f96fdcf8717b00d2ea723d
Drew Wyatt <Drew.Wyatt@activenetwork.com>
2015-08-09 12:08:42 -0500

    all tests passing.

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -7,14 +7,18 @@
     return this.double(1, number);
   };
 
-  Grains.prototype.double = function(number, times, add) {
-    add = (add) ? 1 : 0;
+  Grains.prototype.double = function(number, times, total) {
+    var add = (total) ? number : 0;
 
     if(times === 1) {
       return number;
     } else {
-      return add + this.double(number * 2, times - 1);
+      return add + this.double(number * 2, times - 1, total);
     }
+  };
+
+  Grains.prototype.total = function() {
+    return this.double(1, 64, true);
   };
 
   module.exports = Grains;
