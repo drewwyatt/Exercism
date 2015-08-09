da80db7a89e2f48665f94bf1c64ac2efaa25acac
Drew Wyatt <Drew.Wyatt@activenetwork.com>
2015-08-09 00:02:59 -0500

    added add function

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -9,5 +9,13 @@
     return this.students;
   };
 
+  School.prototype.add = function(name, grade) {
+    if(this.students[grade] !== undefined) {
+      this.students[grade].push(name);
+    } else {
+      this.students[grade] = [name];
+    }
+  };
+
   module.exports = School;
 })();
