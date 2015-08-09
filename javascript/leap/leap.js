ba290da24f2a35badc8eb2f213cab0fbbe9d000f
Drew Wyatt <Drew.Wyatt@activenetwork.com>
2015-08-09 00:31:24 -0500

    isLeapYear passes all tests

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,9 +1,28 @@
 (function() {
   'use strict';
 
-  var isLeapYear = function() {
-    return false;
+  var isLeapYear = function(year) {
+    var isLeapYear = false;
+
+    if(year % 4 === 0) {
+      if(year % 100 === 0) {
+        if(year % 400 === 0) {
+          isLeapYear = true;
+        }
+      } else {
+        isLeapYear = true;
+      }
+    }
+
+    return isLeapYear;
   };
 
   module.exports = isLeapYear;
 })();
+
+
+// ```plain
+// on every year that is evenly divisible by 4
+//   except every year that is evenly divisible by 100
+//     unless the year is also evenly divisible by 400
+// ```
