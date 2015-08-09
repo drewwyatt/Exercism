e5e70aba004cbf2983ff9805138866cedaceae81
Drew Wyatt <Drew.Wyatt@activenetwork.com>
2015-08-09 00:21:45 -0500

    robot-name passing all tests

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -2,7 +2,31 @@
   'use strict';
 
   var Robot = function() {
-    this.name = '';
+    this.reset();
+  };
+
+  Robot.prototype.reset = function() {
+    this.name = NameFactory.makeName();
+  };
+
+  var NameFactory = {};
+
+  NameFactory.makeName = function() {
+    return [
+      this._randomLetter(),
+      this._randomLetter(),
+      this._randomNumber(),
+      this._randomNumber(),
+      this._randomNumber()
+    ].join('').toUpperCase();
+  };
+
+  NameFactory._randomLetter = function() {
+    return String.fromCharCode(97 + Math.floor(Math.random() * 26));
+  };
+
+  NameFactory._randomNumber = function() {
+    return Math.floor(Math.random() * (9 - 0 + 1));
   };
 
   module.exports = Robot;
