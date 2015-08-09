2f1738800c312873b43827543386e31cab501bf8
Drew Wyatt <Drew.Wyatt@activenetwork.com>
2015-08-09 11:32:02 -0500

    earth years working.

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,5 +3,9 @@
 
   var Age = function(seconds) { this.seconds = seconds; };
 
+  Age.prototype.onEarth = function() {
+    return Math.round((this.seconds / 60 / 60 / 24 / 365.25) * 100)  / 100;
+  };
+
   module.exports = Age;
 })();
