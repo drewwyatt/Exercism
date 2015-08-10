8f40b760ce1d33df3e5bab6f3658fc99d8b7d9ee
Drew Wyatt <Drew.Wyatt@activenetwork.com>
2015-08-09 23:04:17 -0500

    reorganized and added threshold math to Numeral

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,48 +1,73 @@
 (function() {
   'use strict';
 
+  /**
+   * Numeral
+   * (Helper object for Romanizer)
+   */
+
+  var Numeral = function(value, threshold) {
+    this.value = value;
+    this.threshold = this._getThreshold(value);
+  };
+
+  Numeral.prototype._getThreshold = function(value) {
+    var threshold = 0.8;
+    if (value === 1 || value % 5 === 0) {
+      threshold = 1;
+    } else if (value % 9 === 0) {
+      threshold = 0.9;
+    }
+
+    return threshold;
+  };
+
+  /**
+   * Romanizer
+   * (all the real work happens here)
+   */
+
   var Romanizer = function(number) {
     this.number = number;
   };
 
-  var Numeral = function(value, threshold) {
-    this.value = value;
-    this.threshold = threshold;
+  // public
+
+  Romanizer.prototype.convert = function() {
+    return this._buildString(this.number, '');
   };
 
   Romanizer.prototype.numerals = {
-    M: new Numeral(1000, 1),
-    CM: new Numeral(900, 0.9),
-    D: new Numeral(500, 1),
-    CD: new Numeral(400, 0.8),
-    C: new Numeral(100, 1),
-    XC: new Numeral(90, 0.9),
-    L: new Numeral(50, 1),
-    XL: new Numeral(40, 0.8),
-    X: new Numeral(10, 1),
-    IX: new Numeral(9, 0.9),
-    V: new Numeral(5, 1),
-    IV: new Numeral(4, 0.8),
-    I: new Numeral(1, 1)
+    M: new Numeral(1000),
+    CM: new Numeral(900),
+    D: new Numeral(500),
+    CD: new Numeral(400),
+    C: new Numeral(100),
+    XC: new Numeral(90),
+    L: new Numeral(50),
+    XL: new Numeral(40),
+    X: new Numeral(10),
+    IX: new Numeral(9),
+    V: new Numeral(5),
+    IV: new Numeral(4),
+    I: new Numeral(1)
   };
 
-  Romanizer.prototype.meetsThreshold = function(number, letter) {
+  // private
+
+  Romanizer.prototype._meetsThreshold = function(number, letter) {
     return (Math.floor(number / this.numerals[letter].value * 10) / 10 >= this.numerals[letter].threshold);
   };
 
-
-
-  // Romanizer.prototype.checkingOrder = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-
-  Romanizer.prototype.buildString = function(number, string) {
+  Romanizer.prototype._buildString = function(number, string) {
     var self = this;
     if (number <= 0) {
       return string;
     } else {
       var doChecking = true;
-      for(var letter in self.numerals) {
+      for (var letter in self.numerals) {
         if (doChecking) {
-          if (self.meetsThreshold(number, letter)) {
+          if (self._meetsThreshold(number, letter)) {
             string += letter;
             number -= self.numerals[letter].value;
             doChecking = false;
@@ -50,15 +75,14 @@
         }
       }
 
-      // console.log(number, string);
-
-      return this.buildString(number, string);
+      return this._buildString(number, string);
     }
   };
 
-  Romanizer.prototype.convert = function() {
-    return this.buildString(this.number, '');
-  };
+  /**
+   * toRoman
+   * ( ^^ function wrapper for everything up there ^^ )
+   */
 
   var toRoman = function(number) {
     return new Romanizer(number).convert();
