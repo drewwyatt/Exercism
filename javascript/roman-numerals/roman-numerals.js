ba4fb70f231a0529ba972895393b4c2aba1cf58d
Drew Wyatt <Drew.Wyatt@activenetwork.com>
2015-08-09 22:39:11 -0500

    all tests passing.

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -13,8 +13,11 @@
     X: 10,
     XL: 40,
     L: 50,
+    XC: 90,
     C: 100,
+    CD: 400,
     D: 500,
+    CM: 900,
     M: 1000
   };
 
@@ -35,7 +38,13 @@
     IX: 0.9,
     X:  1,
     XL: 0.8,
-    L:  1
+    L:  1,
+    XC: 0.9,
+    C: 1,
+    CD: 0.8,
+    D: 1,
+    CM: 0.9,
+    M: 1
   };
 
   Romanizer.prototype.meetsThreshold = function(number, letter) {
@@ -43,7 +52,7 @@
     return (Math.floor(number / this.letters[trimmedLetter] * 10)  / 10 >= this.thresholds[letter]);
   };
 
-  Romanizer.prototype.checkingOrder = ['L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  Romanizer.prototype.checkingOrder = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
   Romanizer.prototype.buildString = function(number, string) {
     var self = this;
@@ -61,12 +70,13 @@
         }
       });
 
+      // console.log(number, string);
+
       return this.buildString(number, string);
     }
   };
 
   Romanizer.prototype.convert = function() {
-    // return this.getLetterForNumber(this.number);
     return this.buildString(this.number, '');
   };
 
